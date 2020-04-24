b024cbf8f3162efe1c67a45ab9b5b5a45372083d
pahoffart25 <pahoffart25@gmail.com>
2020-04-24 14:18:23 -0500

    login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Switch, Route, Redirect } from 'react-router-dom'
+import { connect } from 'react-redux'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Navbar from "./Components/Navbar"
+import Login from "./Components/Login"
+import Profile from "./Components/Profile"
+import EditProfile from './Components/EditProfile'
+import FollowerFeed from "./Components/FollowerFeed"
+import SignUp from "./Components/SignUp"
+import TrailsHome from './Components/TrailsHome'
+import TripForm from "./Components/TripForm"
+import AllUsers from "./Components/AllUsers"
+
+import "bootstrap/dist/css/bootstrap.min.css"
+import "shards-ui/dist/css/shards.min.css"
+import './App.css'
+
+class App extends React.Component {
+
+  componentDidMount() {
+    if(localStorage.token) {
+      fetch('http://localhost:3000/profile',{
+        headers: { Authorization: localStorage.token }
+      })
+      .then(res => res.json())
+      .then(res => {
+        if(res.user)
+          this.props.dispatch({ type: 'GET_USER', user: res.user })
+      })
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Navbar />
+        {
+          localStorage.token && !this.props.user.id ?
+          null
+          :
+          <Switch>
+            <Route exact path='/trails' component={TrailsHome} />
+            <Route exact path='/all-users' component={AllUsers} />
+            <Route exact path='/follower-feed' component={FollowerFeed} />
+            <Route path='/login' render={()=> this.props.loggedIn ? <Redirect to='/profile'/> : <Login/> } />
+            <Route path='/profile' render={()=> this.props.loggedIn  ? <Profile/> : <Redirect to='/login'/> } />
+            <Route exact path="/signup" component={SignUp} />
+            <Route exact path="/edit-profile" component={EditProfile} />
+            <Route exact path="/trip-form" component={TripForm} />
+            <Route path='/' render={()=> <Redirect to='/trails' /> } />
+          </Switch>
+        }
+      </div>
+    )
+  }
 }
 
-export default App;
+let mapStateToProps = state => ({ user: state.userReducer.user, loggedIn: state.userReducer.loggedIn })
+export default connect(mapStateToProps)(App)
+
+
+
+
+
+
